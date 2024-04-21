01b77e669abb447c7e53309a7347b41d099890c0
Evans Manyala <evans.manyala@gmail.com>
2024-04-22 02:08:34 +0300

    Adding code logic

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
+class State(BaseModel, Base):
+    """This class defines the State model"""
+    __tablename__ = 'states'
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+
+    # Add relationship to City model
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """Returns the list of City objects linked to the current State"""
+            from models import storage
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
